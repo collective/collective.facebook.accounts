21f7f172f05b8ffffedc05fd66538db13e86cbd2
hvelarde <hector.velarde@gmail.com>
2012-03-09 19:12:20 -0300

    updated package declaration

--- setup.py
+++ setup.py
@@ -2,21 +2,27 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.facebook.accounts',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      long_description=long_description,
       classifiers=[
         "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone facebook oauth',
       author='Franco Pellegrini',

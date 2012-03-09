d23d8cb206c822973233db613d10d51736f54150
hvelarde <hector.velarde@gmail.com>
2012-03-09 20:28:42 -0300

    fixed i18n and updated Spanish translation

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 
+PATH=bin:$PATH
 I18NDOMAIN="collective.facebook.accounts"
+BASE_DIRECTORY="src/collective/facebook/accounts"
 
 # Synchronise the templates and scripts with the .pot.
-# All on one line normally:
-i18ndude rebuild-pot --pot src/collective/facebook/accounts/locales/${I18NDOMAIN}.pot \
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
-   .
+    ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
-for po in src/collective/facebook/accounts/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    i18ndude sync --pot src/collective/facebook/accounts/locales/${I18NDOMAIN}.pot $po
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
+
+# Report of errors and suspect untranslated messages
+i18ndude find-untranslated ${BASE_DIRECTORY}

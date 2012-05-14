d07a9ef07a28fd260f71692cafe9cd34eff1fb3e
Franco Pellegrini <frapell@gmail.com>
2012-05-14 16:58:59 -0300

    Workaround for removing the configlet, since the GS way does not currently work. closes gh-1

--- src/collective/facebook/accounts/Extensions/Install.py
+++ src/collective/facebook/accounts/Extensions/Install.py
@@ -4,12 +4,19 @@ def install(portal, reinstall=False):
     setup_tool = getToolByName(portal, 'portal_setup')
     if not reinstall:
         setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.accounts:initial')
-        
+
     setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.accounts:default')
-    return "Ran all uninstall steps."
-    
+    return "Ran all install steps."
+
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = getToolByName(portal, 'portal_setup')
         setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.accounts:uninstall')
+
+        # Apparently configlets are not unregistered using just GS, so we do it
+        # here
+
+        portal_conf=getToolByName(portal,'portal_controlpanel')
+        portal_conf.unregisterConfiglet('FacebookSettings')
+
         return "Ran all uninstall steps."
